f5d06c9ae61a4841fce189bc03d507501a1bdb31
YuDo-fsd <y.doloban@gmail.com>
2023-05-07 20:15:35 +0200

    hw3-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FliendList';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 export default function App() {
   return (
@@ -17,6 +19,7 @@ export default function App() {
         likes={user.stats.likes}
       />
       <Statistics items={data} />
+      <FriendList items={friends} />
     </div>
   );
 }
--- src/components/FriendList/FliendList.jsx
+++ src/components/FriendList/FliendList.jsx
@@ -1,1 +1,36 @@
 import PropTypes from 'prop-types';
+
+import css from './FriendList.module.css';
+
+export default function FriendList({ items }) {
+  return (
+    <section className="friendsSection">
+      <ul className="friend-list">
+        {items.map(item => (
+          <li key={item.id} className={css.item}>
+            <p className="status">
+              {item.isOnline === true ? (
+                <span>&#128994;</span>
+              ) : (
+                <span>&#128308;</span>
+              )}
+            </p>
+            <img className={css.avatar} src={item.avatar} alt={item.name} />
+            <p className="name">{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+FriendList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
